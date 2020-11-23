142540c1194c0657068b012e6843962086eba207
roaris <one0803kyopro@gmail.com>
2020-11-24 01:39:45 +0900

    create template.cpp

---
+++ c++/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+using namespace std;
+//using namespace atcoder;
+#define rep(i, n) for (int i=0; i<n; i++)
+#define pb push_back
+typedef long long ll;
+
+int main() {
+    //ループ変数が被っていないか?
+    //制約を確認しているか?
+    //変数のtypoがないか?
+    cin.tie(0); ios::sync_with_stdio(false);
+    
+}
