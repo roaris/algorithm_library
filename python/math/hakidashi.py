a4f42db01f344744751238217a82ffe91663ee6e
roaris <one0803kyopro@gmail.com>
2021-06-04 11:38:35 +0900

    add hakidashi.py

---
+++ python/math/hakidashi.py
@@ -0,0 +1,23 @@
+def hakidashi(A): #mod2の掃き出し法 O(NM^2) 戻り値は(rank, 変換後の行列)
+    N, M = len(A), len(A[0])
+    pos = 0
+    
+    for i in range(M):
+        found = False
+        
+        for j in range(pos, N):
+            if A[j][i]==1:
+                A[pos], A[j] = A[j], A[pos]
+                found = True
+                break
+        
+        if found:
+            for j in range(pos+1, N):
+                if A[j][i]==1:
+                    for k in range(i, M):
+                        A[j][k] ^= A[pos][k]
+            
+            pos += 1
+    
+    return pos, A
+
