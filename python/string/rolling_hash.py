9631b797c428b7a6bc712e3fdcde2200d1532ae2
roaris <61813626+roaris@users.noreply.github.com>
2022-10-15 02:53:34 +0900

    Update rolling_hash.py

--- python/string/rolling_hash.py
+++ python/string/rolling_hash.py
@@ -4,22 +4,22 @@ input = sys.stdin.readline
 from collections import *
 
 class RollingHash:
-    def __init__(self, a):
-        self.base = 31
-        self.mod = 10**9+7
+    def __init__(self, s, base=31, mod=10**9+7):
+        self.base = base
+        self.mod = mod
         self.acc = [0]
         self.power = [1]
         
-        for i in range(len(a)):
-            self.acc.append((self.acc[-1]*self.base%self.mod+a[i])%self.mod)
+        for i in range(len(s)):
+            self.acc.append((self.acc[-1]*self.base%self.mod+ord(s[i])-ord('a')+1)%self.mod)
             self.power.append(self.power[-1]*self.base%self.mod)
     
     def get(self, l, r):
         return (self.acc[r]-self.acc[l]*self.power[r-l])%self.mod
-    
+
 N, Q = map(int, input().split())
 S = input()[:-1]
-rh = RollingHash([ord(c)-ord('a') for c in S])
+rh = RollingHash(S)
 
 for _ in range(Q):
     a, b, c, d = map(int, input().split())
