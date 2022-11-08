b0387832cf19fc6d73d1fc59ecea09369d529714
roaris <61813626+roaris@users.noreply.github.com>
2022-11-09 03:48:07 +0900

    Update segment_tree.cpp

--- c++/data_structure/segment_tree.cpp
+++ c++/data_structure/segment_tree.cpp
@@ -2,65 +2,145 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i=0; i<n; i++)
+#define pb push_back
 typedef long long ll;
-//const ll INF = 10000000000;
-//const ll MOD = 1000000007;
 
-//二項演算は結合法則が成り立てばOK
+template <class S, S (*op)(S, S), S (*e)()>
 struct segtree {
-    int size;
-    vector<ll> node;
-    
-    segtree(int n) {
-        size = 1;
-        while (size<n) size *= 2;
-        node.resize(2*size-1);
-        //ここ初期値にする
-        fill(node.begin(), node.end(), 2147483647);
-    }
-    
-    //k番目(0-indexed)の値をaにする
-    void update(int k, int a) {
-        k += size-1;
-        node[k] = a;
-        while (k>0) {
-            k = (k-1)/2;
-            //ここ変える
-            node[k] = min(node[2*k+1], node[2*k+2]);
+  public:
+    segtree() : segtree(0) {}
+    segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
         }
     }
-    
-    ll query(int a, int b, int k, int l, int r) {
-        //ここ単位元にする
-        if (r<=a || b<=l) return 2147483647;
-        if (a<=l && r<=b) return node[k];
-        else {
-            ll vl = query(a, b, 2*k+1, l, (l+r)/2);
-            ll vr = query(a, b, 2*k+2, (l+r)/2, r);
-            //ここ変える
-            return min(vl, vr);
-        }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
     }
-    
-    //[a, b)に対するクエリ
-    ll get(int a, int b) {
-        return query(a, b, 0, 0, size);
+
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+
+    int ceil_pow2(int n) {
+        int x = 0;
+        while ((1U << x) < (unsigned int)(n)) x++;
+        return x;
     }
 };
 
+int op(int x, int y) {
+    return min(x, y);
+}
+
+int e() {
+    return 2147483647;
+}
+
 int main() {
-    int n, q; cin >> n >> q;
-    segtree st(n);
+    cin.tie(0); ios::sync_with_stdio(false);
     
-    rep(i, q) {
-        int f; cin >> f;
-        if (f==0) {
-            int x, y; cin >> x >> y;
-            st.update(x, y);
-        }
-        else {
-            int x, y; cin >> x >> y;
-            cout << st.get(x, y+1) << endl;
+    int n, q; cin >> n >> q;
+    
+    vector<int> a;
+    rep(i, n) a.pb(2147483647);
+    segtree<int, op, e> st(a);
+    
+    while (q--) {
+        int t; cin >> t;
+        if (t==0) {
+            int i, x; cin >> i >> x;
+            st.set(i, x);
+        } else {
+            int l, r; cin >> l >> r;
+            cout << st.prod(l, r+1) << endl;
         }
     }
 }
