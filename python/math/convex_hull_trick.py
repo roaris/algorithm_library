70ca7957c6e5539a6f63a728f69c55e3f081da87
roaris <61813626+roaris@users.noreply.github.com>
2023-03-16 07:10:31 +0900

    Update convex_hull_trick.py

--- python/math/convex_hull_trick.py
+++ python/math/convex_hull_trick.py
@@ -1,38 +1,79 @@
-#EDPC Z
+# https://yukicoder.me/problems/no/913
 import sys
 input = sys.stdin.readline
 from collections import *
 
-class CHT: #傾きの単調性と最小値クエリのxの単調性を仮定
+class CHT: # 傾きの単調減少と最小値クエリのxの単調性を仮定
     def __init__(self):
-        self.q = deque([]) #直線群
+        self.q = deque([]) # 直線群
         
-    def f(self, f1, x): #直線f1のxの値
+    def f(self, f1, x): # 直線f1のxの値
         return f1[0]*x+f1[1]
     
-    def check(self, f1, f2, f3): #f2を削除しても良いかの判定
+    def check(self, f1, f2, f3): # f2を削除しても良いかの判定
         return (f2[0]-f1[0])*(f3[1]-f2[1])>=(f2[1]-f1[1])*(f3[0]-f2[0])
     
-    def add_line(self, a, b): #傾きa, 切片bの直線を追加
+    def add_line(self, a, b): # 傾きa, 切片bの直線を追加
         while len(self.q)>=2 and self.check(self.q[-2], self.q[-1], (a, b)):
             self.q.pop()
         
         self.q.append((a, b))
     
-    def get(self, x): #xでの最小値
+    def get_inc(self, x): # xでの最小値(xは単調増加)
         while len(self.q)>=2 and self.f(self.q[0], x)>=self.f(self.q[1], x):
             self.q.popleft()
         
         return self.f(self.q[0], x)
     
-N, C = map(int, input().split())
-h = list(map(int, input().split()))
-dp = [0]*N
-cht = CHT()
-cht.add_line(-2*h[0], h[0]**2)
+    def get_dec(self, x): # xでの最小値(xは単調減少)
+        while len(self.q)>=2 and self.f(self.q[-1], x)>=self.f(self.q[-2], x):
+            self.q.pop()
+        
+        return self.f(self.q[-1], x)
 
-for i in range(1, N):
-    dp[i] = h[i]**2+C+cht.get(h[i])
-    cht.add_line(-2*h[i], dp[i]+h[i]**2)
+def dfs(l, r): # [l, r)
+    global ans
+    
+    if l>=r:
+        return
 
-print(dp[N-1])
+    m = (l+r)//2
+    # [l, m)
+    cht = CHT()
+    
+    for i in range(m+1, r+1):
+        cht.add_line(-2*i, i*i+acc[i])
+    
+    res = 10**18
+    
+    for i in range(l, m):
+        res = min(res, cht.get_inc(i)+i*i-acc[i])
+        ans[i] = min(ans[i], res)
+    
+    # [m, r)
+    cht = CHT()
+    
+    for i in range(l, m+1):
+        cht.add_line(-2*i, i*i-acc[i])
+    
+    res = 10**18
+    
+    for i in range(r, m, -1):
+        res = min(res, cht.get_dec(i)+i*i+acc[i])
+        ans[i-1] = min(ans[i-1], res)
+    
+    dfs(l, m)
+    dfs(m+1, r)
+    
+N = int(input())
+A = list(map(int, input().split()))
+acc = [0]
+
+for Ai in A:
+    acc.append(acc[-1]+Ai)
+
+ans = [10**18]*N
+dfs(0, N)
+
+for i in range(N):
+    print(ans[i])
